710f8d388fede1268278219527240c9da433b8df
Olga Simankova <90651122+olgaSimankova@users.noreply.github.com>
2022-07-12 14:59:56 +0300

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+`declare module '*' {
+    const value: string;
+    export default value;
+}
+`
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg';
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            use: [
+                {
+                    require: 'file-loader',
+                },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, './src/js')
+        },
+        fallback: { "path": false }
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        // new copyWebpackPlugin({
+        //     patterns: [
+        //         { from: path.resolve(__dirname, './src/assets/icons'), to: path.resolve(__dirname, './dist/assets/icons') },
+        //         { from: path.resolve(__dirname, './src/assets/books'), to: path.resolve(__dirname, './dist/assets/books') },                
+        //       ],
+        // })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
